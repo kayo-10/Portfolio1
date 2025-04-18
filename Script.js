08f0bc81df51f9b04f81d04080cc5d27682e9faa
kayo-10 <kayofabio26@gmail.com>
2025-04-18 08:28:52 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,23 @@
+const camisasDestaque = [
+  {
+    nome: "Camisa Brasil Neymar Junior",
+    imagem: "https://acdn-us.mitiendanube.com/stores/001/865/527/products/design-sem-nome-111-4c92090aa4b62e75e416714180944641-640-0.png",
+    link: "#"
+  },
+  {
+    nome: "Camisa Al Nassr Cristiano Ronaldo",
+    imagem: "https://acdn-us.mitiendanube.com/stores/001/562/869/products/315-camisa-al-nassr-2022-cr7-cristiano-ronaldo-personalizada-numero-7-amarelo-arabia-saudita-nova1-79696338902af1ab4816729193976685-1024-1024.png",
+    link: "#"
+  },
+  {
+    nome: "Camisa Argentina Lionel Messi",
+    imagem: "https://acdn-us.mitiendanube.com/stores/001/865/527/products/photo5170686599552412318-removebg-preview1-855df5a686afd6d8ad16687986109222-640-0.png",
+    link: "#"
+  }
+];
 
+const outrasCamisas = [
+  { nome: "Camisa São Paulo 2024", imagem: "https://newbrasil.vtexassets.com/arquivos/ids/166079-800-auto?v=638427334085130000&width=800&height=auto&aspect=true", link: "#" },
+  { nome: "Camisa Grêmio 2024", imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYD5Z2g7kTDPa7BeNWhdNsQgfngI2RWJlnxJ-Eg6ZKh_5-vBUHW7cgMvYW&s=10", link: "#" },
+  { nome: "Camisa Internacional 2024", imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStAAFNaAi5aE86x6RnYOH9NbrUiOFy8gb_L7DmHQV4g19l_i_Qe0gUZrI&s=10", link: "#" },
+  { nome: "Camisa Vasco 2024", imagem: "https://encrypted-tbn0.gstatic.com
